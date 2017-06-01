e53f517bef60d580c85d4d683da29994da4ff0a0
kyle-mcnulty <kylemcnulty@gmail.com>
2017-06-01 01:21:53 -0700

    ratings testing

--- Shred.io/app/src/main/java/com/kjmcnult/uw/edu/shredio/DetailsActivity.java
+++ Shred.io/app/src/main/java/com/kjmcnult/uw/edu/shredio/DetailsActivity.java
@@ -98,21 +98,20 @@ public class DetailsActivity extends AppCompatActivity{
                         ratingBar.setRating((float)averageRating);
 
                         // check if the user has already left a rating
-                        if (!skatespot.getUserRatings().keySet().contains(user.getEmail())) {
-                            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-                                @Override
-                                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                            @Override
+                            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                                if (!skatespot.getUserRatings().keySet().contains(user.getEmail())) {
                                     HashMap<String, Double> userRatings = skatespot.getUserRatings();
                                     userRatings.put(user.getEmail().replace(".", ""), (double) rating);
                                     averageRating = getRating(userRatings);
                                     ratingBar.setRating((float) averageRating);
                                     skatespot.setUserRatings(userRatings);
                                     ref.child(markerKey).setValue(skatespot);
-                                    Toast.makeText(getApplicationContext(), "You submitted a rating!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "You submitted a rating of: " + rating, Toast.LENGTH_SHORT).show();
                                 }
-                            });
-                        }
-
+                            }
+                        });
 
                         TextView name = (TextView) findViewById(R.id.spot_name);
                         name.setText(skatespot.getName());
